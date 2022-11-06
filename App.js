7bf785078c26e359f2fbb204437401b12222a75e
mp000000 <110737604+mp000000@users.noreply.github.com>
2022-11-06 15:29:55 +0100

    Update App.js

--- App.js
+++ App.js
@@ -17,7 +17,6 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { MaterialIcons } from "@expo/vector-icons";
-import SearchableDropDown from "react-native-searchable-dropdown";
 
 const Tab = createBottomTabNavigator();
 
